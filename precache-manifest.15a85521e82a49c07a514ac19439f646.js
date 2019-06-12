821b486a86b712d31d6185d9c1ec2e60fee329dc
robber <robber995@gmail.com>
2019-06-13 01:37:10 +0300

    Updates

---
+++ precache-manifest.15a85521e82a49c07a514ac19439f646.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7d641ce72cce0ee1c1877b728050d4",
+    "url": "/liubomyrfedoryshyn.github.io/teamvoyapp/index.html"
+  },
+  {
+    "revision": "4f5fd6d060a79fd29641",
+    "url": "/liubomyrfedoryshyn.github.io/teamvoyapp/static/css/main.e2642836.chunk.css"
+  },
+  {
+    "revision": "7c2c3fdfdfbe774ca969",
+    "url": "/liubomyrfedoryshyn.github.io/teamvoyapp/static/js/2.e20ef66d.chunk.js"
+  },
+  {
+    "revision": "4f5fd6d060a79fd29641",
+    "url": "/liubomyrfedoryshyn.github.io/teamvoyapp/static/js/main.d96c20da.chunk.js"
+  },
+  {
+    "revision": "5d30b8369a453648ad87",
+    "url": "/liubomyrfedoryshyn.github.io/teamvoyapp/static/js/runtime~main.f349c9e6.js"
+  }
+]);
