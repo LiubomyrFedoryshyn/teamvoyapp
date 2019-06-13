9921c9d9c1aa4326b11f42d1346f868499406abf
robber <robber995@gmail.com>
2019-06-13 09:06:18 +0300

    Updates

---
+++ precache-manifest.29f98c4b839c010663a51286a8b8e994.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ba40d4e658d0227a45a198171ce536",
+    "url": "/teamvoyapp/index.html"
+  },
+  {
+    "revision": "0a83eb43b4307a330340",
+    "url": "/teamvoyapp/static/css/main.e2642836.chunk.css"
+  },
+  {
+    "revision": "7c2c3fdfdfbe774ca969",
+    "url": "/teamvoyapp/static/js/2.e20ef66d.chunk.js"
+  },
+  {
+    "revision": "0a83eb43b4307a330340",
+    "url": "/teamvoyapp/static/js/main.a8d4b344.chunk.js"
+  },
+  {
+    "revision": "963fa5d12b4ba8bbcf96",
+    "url": "/teamvoyapp/static/js/runtime~main.99ce054d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/liubomyrfedoryshyn.github.io/teamvoyapp/precache-manifest.6a795bc3c90665e6e06f56e29075347a.js"
+  "/teamvoyapp/precache-manifest.29f98c4b839c010663a51286a8b8e994.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/liubomyrfedoryshyn.github.io/teamvoyapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teamvoyapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
